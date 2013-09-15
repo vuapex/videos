90dccd792a8395dd38524898134cb52861ddf08d
Farrukh <farrukh@vuapex.com>
2013-09-16 00:19:10 +0500

    a

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<?
+<html>
+<head>
+<form action="dnlUTube.php" method="post">
+<p><b>YouTube URL:</b> <input size= 28 type="text" name="name" /></p>
+<p><input type="submit" value="Get it!" /></p>
+</form>
+<i>Example: http://www.youtube.com/watch?v=319hsZ12TAE</i>
+</head>
+<body>
 
-
-
-
-?>
+</body>
+</html>
