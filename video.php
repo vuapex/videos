1245ce7401e6f401a4f56de80e542bcd0f196ff0
Farrukh <farrukh@vuapex.com>
2013-09-20 01:38:50 +0500

    gfh

---
+++ video.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html 
+  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>Recent Videos | Video By Choice</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<script type="text/javascript" src="js/jwplayer/jwplayer.js"></script>
+<script type="text/javascript">jwplayer.key="Ksio49Ksi8bdi8KS8S";</script>
+<script src="http://jwpsrv.com/library/eRPQyiEnEeO7hCIACusDuQ.js"></script>
+  </head>
+  <body>
+        <div style="display: none; position: absolute; z-index: 110; left: 400; top: 100; width: 15; height: 15" id="preview_div"></div>
+        <table id="main">
+            <tbody><tr>
+    <td colspan="3" class="header">
+        <div id="home-link">
+        </div>
+        <div id="search-div">
+            <form action="/index.php">
+                <input type="text" id="search" name="cat">
+                <input type="submit" value="Search">
+            </form>
+        </div>
+    </td>
+</tr>
+<tr>
+    <td colspan="3" class="header-shade"></td>
+</tr>
+                <tr>
+                    <td>
+                        <div id="myElement">Loading the player...</div>
+
+<script type="text/javascript">
+    jwplayer("myElement").setup({
+        file: "/uploads/myVideo.mp4",
+        image: "/uploads/myPoster.jpg"
+    });
+</script>
+                    </td>
+                </tr>
+            <tr>
+    <td colspan="3" class="footer">
+        Copyright &copy; <a href="#">www.videobychoice.com</a>
+
+        
+        </tbody></table>
+    
+</body>
+</html> 
